f4d6c8120a09e2dc9733f7eb4c32d0cfe3322e74
kylanalicia <aliciakimani.ky@gmail.com>
2025-07-14 08:53:13 +0300

    Completed the creating the header

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets'
+
+const ExploreMenu = () => {
+  return (
+    <div className='explore-menu' id='explore-menu'>
+        <h1>Explore our menu</h1>
+        <p>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to crafted with the finest ingredients and culinary expertise.</p>
+
+    </div>
+  )
+}
+
+export default ExploreMenu

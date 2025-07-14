14e0d571c75f50fab7e54ae8f2ea999b90c0ac1d
kylanalicia <aliciakimani.ky@gmail.com>
2025-07-14 12:02:28 +0300

    Completed the footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,12 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
-    <div className='App'>
+    <>
+     <div className='App'>
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -16,6 +18,9 @@ const App = () => {
       </Routes>
 
     </div>
+    <Footer/>
+    </>
+   
   )
 }
 
